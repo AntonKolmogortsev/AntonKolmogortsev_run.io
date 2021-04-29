1c726aa97eb21ede05265d7eca088ab0ce89401a
unknown <akweb.pro777@gmail.com>
2021-04-29 13:11:37 +0300

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,19 @@
 'use strict';
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    speed: 2500,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../img/slidericons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../img/slidericons/right.svg"></button>',
+    responsive: [
+        {
+                    breakpoint: 992,
+                settings: {
+                dots: false,
+                arrows: false
+            }
+        }
+    ]
+  });
+});
 
